8f75660c425ed3223eee7d58b4edb698102c67b0
FunmbiO <oolajubu01@mynbcc.ca>
2026-03-16 08:04:28 -0300

    Allow /videos & /webjars; unify home mapping
    Add /videos/** and /webjars/** to SecurityConfig static resource whitelist so those assets are served without authentication. Replace class-level @RequestMapping in HomeController with a single @GetMapping("/") and remove the duplicated home() handler from EventViewController to centralize the home endpoint.

--- Restaurant-Project/common/common-web/src/main/java/nbcc/common/controller/HomeController.java
+++ Restaurant-Project/common/common-web/src/main/java/nbcc/common/controller/HomeController.java
@@ -1,14 +1,14 @@
 package nbcc.common.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home(){
+    @GetMapping("/")
+    public String home() {
         return "home";
     }
 }
